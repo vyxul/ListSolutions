c707849190a0c32aa2b96783a6c49d5b4e5a6442
vyxul <33010901+vyxul@users.noreply.github.com>
2018-11-18 23:42:14 -0600

    Add files via upload

--- ListSolutions.java
+++ ListSolutions.java
@@ -10,32 +10,32 @@ public class ListSolutions {
         System.out.println("X1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 + X9 = 12");
         System.out.println();
         
-        for (i0 = 1; i0 <= finalSum - 8; i0++, array[0] = i0) {
-            //array[0] = i0;
+        for (i0 = 1; i0 <= finalSum - 8; i0++) {
+            array[0] = i0;
 
-            for (i1 = 1; i1 <= finalSum - 8; i1++, array[1] = i1) {
-                //array[1] = i1;
+            for (i1 = 1; i1 <= finalSum - 8; i1++) {
+                array[1] = i1;
 
-                for (i2 = 1; i2 <= finalSum - 8; i2++, array[2] = i2) {
-                    //array[2] = i2;
+                for (i2 = 1; i2 <= finalSum - 8; i2++) {
+                    array[2] = i2;
 
-                    for (i3 = 1; i3 <= finalSum - 8; i3++, array[3] = i3) {
-                        //array[3] = i3;
+                    for (i3 = 1; i3 <= finalSum - 8; i3++) {
+                        array[3] = i3;
 
-                        for (i4 = 1; i4 <= finalSum - 8; i4++, array[4] = i4) {
-                            //array[4] = i4;
+                        for (i4 = 1; i4 <= finalSum - 8; i4++) {
+                            array[4] = i4;
 
-                            for (i5 = 1; i5 <= finalSum - 8; i5++, array[5] = i5) {
-                                //array[5] = i5;
+                            for (i5 = 1; i5 <= finalSum - 8; i5++) {
+                                array[5] = i5;
 
-                                for (i6 = 1; i6 <= finalSum - 8; i6++, array[6] = i6) {
-                                    //array[6] = i6;
+                                for (i6 = 1; i6 <= finalSum - 8; i6++) {
+                                    array[6] = i6;
 
-                                    for (i7 = 1; i7 <= finalSum - 8; i7++, array[7] = i7) {
-                                        //array[7] = i7;
+                                    for (i7 = 1; i7 <= finalSum - 8; i7++) {
+                                        array[7] = i7;
 
-                                        for (i8 = 1; i8 <= finalSum - 8; i8++, array[8] = i8) {
-                                            //array[8] = i8;
+                                        for (i8 = 1; i8 <= finalSum - 8; i8++) {
+                                            array[8] = i8;
 
 
                                             if (arraySum(array) == finalSum) {
